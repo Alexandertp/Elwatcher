6c7dad03ef7048875fbbc7b1ee8e268f1189d8d4
Frolunddd <oliver.frolund@gmail.com>
2026-09-09 10:53:05 +0200

    Create EDSController.cs

---
+++ ElWatcher/Controllers/EDSController.cs
@@ -0,0 +1,23 @@
+﻿using ElWatcher.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElWatcher.Controllers;
+
+[ApiController]
+public class EDSController : ControllerBase
+{
+    private readonly IEnergiDataService _energiDataService;
+
+    public EDSController(IEnergiDataService energiDataService)
+    {
+        _energiDataService =  energiDataService;
+    }
+
+    [HttpGet]
+    [Route("Co2Emission")]
+    public async Task<IActionResult> GetCo2Emission()
+    {
+        var data = await _energiDataService.GetCurrentCo2EmissionAsync();
+        return Ok(data);
+    }
+}
