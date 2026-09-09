32b5651589c8add0f51845bfd977870dd619606c
Frolunddd <oliver.frolund@gmail.com>
2026-09-09 10:53:22 +0200

    Create Co2Observationer.cs

---
+++ ElWatcher/Models/Co2Observationer.cs
@@ -0,0 +1,8 @@
+﻿namespace ElWatcher.Models;
+
+public class Co2Observationer
+{
+    public DateTime Tidspunkt { get; set; }
+    public double EmissionValue { get; set; }
+    public string Omraade { get; set; } = string.Empty;
+}
