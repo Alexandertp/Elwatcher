1d6dcaebd51ca5db31177f560756e95a1bc0a818
Frolunddd <oliver.frolund@gmail.com>
2026-09-10 11:04:41 +0200

    change name

--- ElWatcher/Services/DBLagringRepo.cs
+++ ElWatcher/Services/DBLagringRepo.cs
@@ -1,20 +1,22 @@
-﻿using ElWatcher.Models;
+﻿using ElWatcher.Interfaces;
+using ElWatcher.Models;
 using Microsoft.Data.SqlClient;
 
 namespace ElWatcher.Services;
 
-public class DBLagringService
+public class DBLagringRepo : IDBLagring
 {
     private string dataSovs;
     private string userID;
     private string password;
     private string dbCatalog;
-    public DBLagringService(IConfiguration configuration)
+    public DBLagringRepo(IConfiguration configuration)
     {
         dataSovs = configuration["DBCredentials:DataSource"] ?? "localhost";
         userID = configuration["DBCredentials:UserID"] ?? "root";
         password = configuration["DBCredentials:Password"] ?? "root";
         dbCatalog = configuration["DBCredentials:DBCatalog"] ?? "elwatcher";
+        
     }
     private async Task<SqlConnection> ForbindTilDatabase()
     {
@@ -31,8 +33,7 @@ public class DBLagringService
 
         try
         {
-            await using var connection = new SqlConnection(connectionString);
-            
+            var connection = new SqlConnection(connectionString);
             await connection.OpenAsync();
             return connection;
         }
@@ -44,10 +45,19 @@ public class DBLagringService
         return null;
     }
 
-    public async Task GemCo2Emission(IEnumerable<Co2Observationer> data, SqlConnection connection)
+    public async Task GemCo2Emission(IEnumerable<Co2Observationer> data)
     {
-        var sql = "SELECT * FROM Co2Emission";
-        await using var command = new SqlCommand(sql, connection);
+        SqlConnection connection = await ForbindTilDatabase();
+
+        string del1 = "INSERT INTO Co2Emission (EmissionValue, Omraade, Tidspunkt) VALUES";
+        string del2 = "";
+        foreach (var Co2Observationer in data)
+        {
+            del2 += $"('{Co2Observationer.EmissionValue}','{Co2Observationer.Omraade}','{Co2Observationer.Tidspunkt})',";
+        }
+        del2.TrimEnd(',');
+        var stringSql = del1 + del2 + ";";
+        await using var command = new SqlCommand(stringSql, connection);
         await using var reader = await command.ExecuteReaderAsync();
     }
 }
