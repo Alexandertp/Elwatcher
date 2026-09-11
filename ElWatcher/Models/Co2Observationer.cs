ee09f52c79a8eaa0fe3c1b03f6511f07fc86d126
Frolunddd <oliver.frolund@gmail.com>
2026-09-11 11:20:23 +0200

    Delete Co2Observationer.cs

--- ElWatcher/Models/Co2Observationer.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ElWatcher.Models;
-
-public class Co2Observationer
-{
-    public DateTime Tidspunkt { get; set; }
-    public double EmissionValue { get; set; }
-    public string Omraade { get; set; } = string.Empty;
-}
