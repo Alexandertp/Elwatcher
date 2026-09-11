a69c9158640b4891b3de749a6cb6dc3dc97ca28d
Frolunddd <oliver.frolund@gmail.com>
2026-09-11 11:20:16 +0200

    ændre navn på Co2 observationer

---
+++ ElWatcher/Models/Co2Observation.cs
@@ -0,0 +1,8 @@
+﻿namespace ElWatcher.Models;
+
+public class Co2Observation
+{
+    public DateTime Minutes5DK { get; set; }
+    public double CO2Emission { get; set; }
+    public string PriceArea { get; set; } = string.Empty;
+}
