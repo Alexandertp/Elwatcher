5aac404ea9ffc09fb2d1e0e3d3c989e5229c5adb
Frolunddd <oliver.frolund@gmail.com>
2026-09-14 14:11:04 +0200

    lavet så vi kun ser top 6 observationer

--- ElWatcher/Interfaces/IDBLagring.cs
+++ ElWatcher/Interfaces/IDBLagring.cs
@@ -6,4 +6,5 @@ public interface IDBLagring
 {
     Task GemCo2Emission(IEnumerable<Co2Observation> data);
     Task<IEnumerable<Co2Observation>> HentAlleCo2Emission();
+    Task<IEnumerable<Co2Observation>> HentTop6Co2Emission();
 }
